2eda8202c607694bc0f6a771e6bb089c57b5dcfa
ReinhardBatubara <reinhardbatubara607@gmail.com>
2023-06-06 21:22:19 +0700

    finish

---
+++ t01_01.c
@@ -0,0 +1,18 @@
+// NIM - Name
+// NIM - Name
+
+#include <stdio.h>
+
+int main(int _argc, char **_argv) {
+ switch (expression)
+ {
+ case /* constant-expression */:
+  /* code */
+  break;
+ 
+ default:
+  break;
+ }
+
+  return 0;
+}
---
+++ t01_02.c
@@ -0,0 +1,26 @@
+// NIM - Name
+// NIM - Name
+
+#include <stdio.h>
+
+int main(int _argc, char **_argv) {
+ 
+ unsigned int ucok, butet, nucok, nbutet, wucok, wbutet;
+
+ scanf("%d", &ucok);
+ scanf("%d", &butet);
+
+while (/* condition */)
+{
+  /* code */
+}
+
+
+ if (ucok>2)
+{
+  ucok=ucok-3;
+  nucok=nucok+1;
+}
+
+  return 0;
+}
